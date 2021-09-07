2a156a01eddb5d1f4f5b5090654f5989655be562
porkhunkateryna <75451945+porkhunkateryna@users.noreply.github.com>
2021-09-07 21:33:40 +0300

    Add files via upload

---
+++ classwork/cw1/surname.java
@@ -0,0 +1,11 @@
+package classwork;
+import java.util.Scanner;
+public class surname {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("enter surname" );
+	        String surname = scanner.next();
+	        System.out.println("Hello, " + surname);
+	    }
+
+}
